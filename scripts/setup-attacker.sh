95fdbd6acb3967d42ed2546fabe1e11fb6f33756
Gombi <gombartomimi@gmail.com>
2025-12-19 14:08:41 +0100

    changed attacker ip and set victim to use server for dns resolving

--- scripts/setup-attacker.sh
+++ scripts/setup-attacker.sh
@@ -1,6 +1,12 @@
 apt-get update
 apt-get upgrade -y
 
+# Ensure right IPs are assigned via NetworkManager
+nmcli con show "attacker-eth1" >/dev/null 2>&1 || \
+nmcli con add type ethernet ifname eth1 con-name "attacker-eth1" ipv4.method manual ipv4.addresses 192.168.56.10/24 ipv4.gateway "" ipv4.dns "" autoconnect yes
+nmcli con mod "attacker-eth1" ipv4.addresses 192.168.50.10/24 ipv4.method manual ipv4.gateway "" ipv6.method ignore
+nmcli con up "attacker-eth1"
+
 #Install packages
 apt-get install -y \
     python3 \
--- scripts/setup-victim.sh
+++ scripts/setup-victim.sh
@@ -1,2 +1,8 @@
 apt-get update
 apt-get upgrade -y
+
+# Force DNS resolution through the server VM
+systemctl stop systemd-resolved
+systemctl disable systemd-resolved
+rm -f /etc/resolv.conf
+echo "nameserver 192.168.56.30" > /etc/resolv.conf
